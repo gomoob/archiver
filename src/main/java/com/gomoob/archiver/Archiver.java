55493e1e5af361abe270b68c88ff1a0777839cfc
Baptiste Gaillard <baptiste.gaillard@gomoob.com>
2014-04-09 09:08:30 +0200

    Fix loading of the archiver configuration file when using the archiver install.

--- src/main/java/com/gomoob/archiver/Archiver.java
+++ src/main/java/com/gomoob/archiver/Archiver.java
@@ -19,6 +19,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
 import java.util.Arrays;
 
 import org.apache.commons.cli.CommandLine;
@@ -46,7 +47,7 @@ public class Archiver {
      * Main entry of the GOMOOB AWS Glacier backup tool.
      * 
      * @param args arguments passed to the command line.
-     * @throws CommandException 
+     * @throws CommandException
      */
     @SuppressWarnings("static-access")
     public static void main(String[] args) throws CommandException {
@@ -75,21 +76,25 @@ public class Archiver {
         options.addOption(aConfigurationFile);
         options.addOption(glacierOption);
         options.addOption(helpOption);
-        
+
         CommandLineParser commandLineParser = new PosixParser();
 
         try {
-            
+
             String[] firstArgs = new String[] {};
-            
-            if(args.length == 1) {
-                
+
+            if (args.length == 1) {
+
                 firstArgs = Arrays.copyOfRange(args, 0, 1);
-                
-            } else if(args.length > 1) {
-                
+
+            } else if (args.length > 1 && args[1].trim().toLowerCase().equals("a-configuration-file")) {
+
                 firstArgs = Arrays.copyOfRange(args, 0, 2);
-                
+
+            } else if (args.length > 1) {
+
+                firstArgs = Arrays.copyOfRange(args, 0, 1);
+
             }
 
             CommandLine commandLine = commandLineParser.parse(options, firstArgs);
@@ -106,43 +111,103 @@ public class Archiver {
 
                 ConfigurationParser configurationParser = new ConfigurationParser();
                 InputStream configurationStream = null;
-                
+
                 // Loads a custom archiver configuration file
-                if(commandLine.hasOption("a-configuration-file")) {
-                    
-                    configurationStream = new FileInputStream(new File(commandLine.getOptionValue("a-configuration-file")));
-                    
-                } 
-                
+                if (commandLine.hasOption("a-configuration-file")) {
+
+                    configurationStream = new FileInputStream(new File(
+                            commandLine.getOptionValue("a-configuration-file")));
+
+                }
+
                 // Loads the default configuration file
                 else {
-                
-                    configurationStream = Archiver.class.getResourceAsStream("/configuration.json");
-                    
+
+                    try {
+
+                        // Returns a string like
+                        // 'jar:file:/C:/install_dir/lib/archiver-x.y.z.jar!/com/gomoob/archiver/Archiver.class
+                        String archiverClassUri = Archiver.class.getResource("/com/gomoob/archiver/Archiver.class")
+                                .toURI().toString();
+
+                        // If the the 'Archiver.class' is declared inside an archiver JAR file
+                        if (archiverClassUri.startsWith("jar:file:")) {
+
+                            // Gets the file path to the archiver JAR file on the file system, for example
+                            // '/C:/install_dir/archiver-x.y.z.jar'
+                            String jarFilePath = archiverClassUri.substring(9, archiverClassUri.indexOf("!") + 1);
+
+                            // Gets the parent dir of the JAR file, this should be the 'dir' directory of the archiver
+                            // install
+                            File jarFileParentDir = new File(jarFilePath).getParentFile();
+
+                            // If we are inside a 'lib' directory
+                            if (jarFileParentDir.getName().equals("lib")) {
+
+                                // Gets the archiver 'conf' directory
+                                File confDir = new File(jarFileParentDir.getParentFile(), "conf");
+                                File configurationFile = null;
+
+                                if (confDir.exists() && confDir.isDirectory()) {
+
+                                    configurationFile = new File(confDir, "archiver.json");
+
+                                    // Cannot find the configuration file
+                                    if (!configurationFile.exists()) {
+
+                                        throw new IllegalStateException("Cannot find the 'archiver.json' file !");
+
+                                    }
+
+                                    configurationStream = new FileInputStream(configurationFile);
+
+                                } else {
+
+                                    throw new IllegalStateException("Can't find the Archiver 'conf' directory !");
+
+                                }
+
+                            }
+
+                            // The Archiver JAR file is not declared in the right directory
+                            else {
+
+                                throw new IllegalStateException(
+                                        "The archiver JAR is not declared inside a 'lib' directory !");
+
+                            }
+
+                        }
+
+                    } catch (URISyntaxException use) {
+                        // TODO Auto-generated catch block
+                        use.printStackTrace();
+                    }
+
                 }
-                
+
                 Configuration configuration = configurationParser.parse(configurationStream);
-                
+
                 if (commandLine.hasOption("glacier")) {
 
                     GlacierPlugin glacierComponent = new GlacierPlugin();
                     glacierComponent.setConfiguration(configuration);
-                    
-                    if(commandLine.hasOption("a-configuration-file")) {
-                    
+
+                    if (commandLine.hasOption("a-configuration-file")) {
+
                         glacierComponent.execute(Arrays.copyOfRange(args, 2, args.length));
 
                     } else {
-                        
+
                         glacierComponent.execute(Arrays.copyOfRange(args, 1, args.length));
-                        
+
                     }
 
                 } else {
-                    
+
                     HelpFormatter helpFormatter = new HelpFormatter();
                     helpFormatter.printHelp(120, "archiver", "", options, "");
-                    
+
                 }
 
             }
